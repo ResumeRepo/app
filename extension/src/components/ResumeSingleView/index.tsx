2daf6c01c7ed3253405a64c444f78027ed7af9c5
Biz Melesse <19600025+bizyb@users.noreply.github.com>
2024-06-11 23:43:09 -0500

    Render pdf in panel

--- extension/src/components/ResumeSingleView/index.tsx
+++ extension/src/components/ResumeSingleView/index.tsx
@@ -19,102 +19,49 @@ export default function ResumeSingleView(props: ResumeSingleViewProps): JSX.Elem
   const [errorMessage, setErrorMessage] = useState(null); // Store error message
   const [downloadUrl, setDownloadUrl] = useState<string | null>(null);
   const componentRef = useRef(null);
+  const [pdfFound, setPdfFound] = useState(false)
 
   const baseUrl = "http://localhost:4000"
   const endpoint = "/convert-to-pdf"
   const savePdf = (html: any) => {
     setIsLoading(true);
     setErrorMessage(null);
-    setDownloadUrl(null); //
-    console.log("debug 1: ", typeof html)
-    let base64EncodedHtml
-    try {
-      const utf8EncodedHtml = new TextEncoder().encode(html);
-      base64EncodedHtml = btoa(String.fromCharCode(...utf8EncodedHtml));
-    } catch (e) {
-      console.log("error...: ", e)
-    }
+    setDownloadUrl(null);
+
+    const utf8EncodedHtml = new TextEncoder().encode(html);
+    const base64EncodedHtml = btoa(String.fromCharCode(...utf8EncodedHtml));
     const data = {
       format: 'Letter',
       html: base64EncodedHtml,
     };
 
-    console.log("debug 2")
-    // const response = await axios.post(`${baseUrl}${endpoint}`, data, {
-    //   responseType: 'blob', // Specify response type as blob
-    // });
-    // console.log('PDF generated successfully!');
-    //
-    // const blob = new Blob([response.data], { type: 'application/pdf' }); // Create Blob
-    // const downloadUrl = URL.createObjectURL(blob); // Create temporary URL
-    // setDownloadUrl(downloadUrl);
-
-
-
-    console.log("making axios request....")
     axios.post(`${baseUrl}${endpoint}`, data, {
       responseType: 'blob',
       headers: { 'Content-Type': 'application/json' }
     })
     .then(response => {
-      console.log('PDF generated successfully!');
       const blob = new Blob([response.data], { type: 'application/pdf' }); // Create Blob
       const downloadUrl = URL.createObjectURL(blob); // Create temporary URL
       setDownloadUrl(downloadUrl);
+      setPdfFound(true)
+      const iframe = document.querySelector("iframe");
+      if (iframe?.src) iframe.src = downloadUrl;
     })
     .catch(error => {
       console.error('Error converting HTML to PDF:', error);
     });
-
-
-
-
-
-
-    // const html = componentRef.current
-    // const html = document.getElementById("resume-preview-0")
-    // console.log(html)
-    // const doc = new jsPDF({
-    //   format: "letter",
-    //   orientation: "p"
-    // })
-    // doc.html(html, {
-    //   callback: function (_doc) {
-    //     // console.log(html)
-    //     _doc.save("demo.pdf")
-    //   }
-    // })
   }
 
   const handlePrint = useReactToPrint({
     documentTitle: "Print This Document",
-    // onBeforePrint: () => console.log("before printing..."),
-    // onAfterPrint: () => console.log("after printing..."),
-
     copyStyles: true,
     print: async (printIframe: HTMLIFrameElement) => {
       const html = printIframe.contentDocument.getElementsByTagName("html")[0]
-      // console.log(html)
       savePdf(`<!DOCTYPE html>
 <html lang="en">${html.innerHTML}</html>\n`)
     },
     removeAfterPrint: true,
   });
-  // console.log(componentRef.current)
-
-  // const handlePrint = useReactToPrint({
-  //   documentTitle: "Print This Document",
-  //   onBeforePrint: () => console.log("before printing..."),
-  //   onAfterPrint: () => console.log("after printing..."),
-  //   copyStyles: false,
-  //   print: async (printIframe: HTMLIFrameElement) => {
-  //     const doc = printIframe.contentDocument
-  //     savePdf(doc.getElementsByTagName("html")[0])
-  //   },
-  //   removeAfterPrint: true,
-  // });
-
-
 
   const onTemplateSelect = (templateId: string) => {
     setTemplateId(templateId)
@@ -161,12 +108,13 @@ export default function ResumeSingleView(props: ResumeSingleViewProps): JSX.Elem
               </Card>
               <div className="exclude-print w-full flex justify-end">
                 <WinPrint onClick={() => handlePrint(null, () => componentRef.current)}/>
-                {downloadUrl && <a href={downloadUrl} download="converted.pdf">
-                  Download PDF
-                </a>}
+                {/*{downloadUrl && <a href={downloadUrl} download="converted.pdf">*/}
+                {/*  Download PDF*/}
+                {/*</a>}*/}
               </div>
+              <iframe src="" width={816} height={1051}/>
               <div className="resume-preview-outer" id="resume-preview-0">
-                  <Preview ref={componentRef}/>
+                {!pdfFound &&  <Preview ref={componentRef}/> }
               </div>
             </div>
         ) : <></>
