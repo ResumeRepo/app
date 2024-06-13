7bf7f58b4c98f9bffdae3a8ecb87dc362d3ab981
Biz Melesse <19600025+bizyb@users.noreply.github.com>
2024-06-13 12:41:40 -0500

    Add file save option

--- extension/src/components/ResumeSingleView/index.tsx
+++ extension/src/components/ResumeSingleView/index.tsx
@@ -13,6 +13,7 @@ import 'react-pdf/dist/Page/TextLayer.css';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import { Document, Page } from 'react-pdf';
 import axios from "axios";
+import { saveAs } from 'file-saver';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
     'pdfjs-dist/build/pdf.worker.js',
@@ -29,7 +30,8 @@ export default function ResumeSingleView(props: ResumeSingleViewProps): JSX.Elem
 
   const baseUrl = "http://localhost:4000"
   const endpoint = "/convert-to-pdf"
-  const savePdf = (html: any) => {
+  const fileName = "Resume.pdf"
+  const generatePdf = (html: any) => {
     setIsLoading(true);
     setErrorMessage(null);
     setDownloadUrl(null);
@@ -61,19 +63,43 @@ export default function ResumeSingleView(props: ResumeSingleViewProps): JSX.Elem
     });
   }
 
-  const handlePrint = useReactToPrint({
-    documentTitle: "Print This Document",
+  const renderPdf = useReactToPrint({
+    documentTitle: "Resume",
     copyStyles: true,
     print: async (printIframe: HTMLIFrameElement) => {
       const html = printIframe.contentDocument.getElementsByTagName("html")[0]
-      savePdf(`<!DOCTYPE html>
+      generatePdf(`<!DOCTYPE html>
 <html lang="en">${html.innerHTML}</html>\n`)
     },
     removeAfterPrint: true,
+    suppressErrors: true,
   });
 
   const onTemplateSelect = (templateId: string) => {
     setTemplateId(templateId)
+  }
+
+  useEffect(() => {
+    renderPdf(null, () => componentRef.current)
+    // renderPdf()
+  }, [componentRef.current]);
+
+  const savePdf = () => {
+    if (downloadUrl) {
+      saveAs(downloadUrl, fileName)
+    }
+  }
+
+  const loadingProgress = () => {
+    return <div className="text-center mt-10 mb-4">
+      <div role="status">
+        <svg aria-hidden="true" className="inline w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z" fill="currentColor"/>
+          <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill"/>
+        </svg>
+        <span className="sr-only">Loading...</span>
+      </div>
+    </div>
   }
 
   return (
@@ -115,20 +141,29 @@ export default function ResumeSingleView(props: ResumeSingleViewProps): JSX.Elem
               <Card>
                 <TemplateCarousel/>
               </Card>
-              <div className="exclude-print w-full flex justify-end">
-                <WinPrint onClick={() => handlePrint(null, () => componentRef.current)}/>
-              </div>
 
 
               <div className="resume-preview-outer" id="resume-preview-0">
-                <Document file={downloadUrl}>
-                  <Page pageNumber={1} />
-                </Document>
+                { downloadUrl ?
+                    <>
+                      <div className="flex justify-end w-full">
+                        <WinPrint onClick={savePdf}/>
+                      </div>
+                      <Document
+                          loading={loadingProgress()}
+                          file={downloadUrl}
+                          onLoadProgress={() => setPdfFound(false)}
+                          onLoadSuccess={() => setPdfFound(true)}
+                          onLoadError={() => setPdfFound(false)}
+                      >
+                        <Page pageNumber={1}/>
+                      </Document>
+
+                    </>:
+                    <>{loadingProgress()}</>
+                }
                 {!pdfFound &&  <Preview ref={componentRef}/> }
               </div>
-
-
-
             </div>
         ) : <></>
       }
