89205b631cc96677aaf6caac17e2c9cf89e40094
Biz Melesse <19600025+bizyb@users.noreply.github.com>
2024-06-26 23:40:54 -0500

    Move frontend to frontend-old

---
+++ frontend-old/app/head.tsx
@@ -0,0 +1,10 @@
+export default function Head() {
+  return (
+    <>
+      <title>NextRole: AI Resume Assistant</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <meta name="description" content="NextRole is a personal AI assistant to help you customize your resume and land your next job." />
+      <link rel="icon" href="/favicon.ico" />
+    </>
+  );
+}
